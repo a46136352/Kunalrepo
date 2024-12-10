c9fab9c064cd6fdd5c5888d26c7387c9b1ce5e7c
ROWDY <excuseboy13@gmail.com>
2024-12-10 16:18:45 +0530

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -91,7 +91,13 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▓" * completed_length + "▒" * remaining_length
             
             try:
-                await reply.edit(f'`\n ╭──⌯════𝐁𝐨𝐭 𝐒𝐭𝐚𝐭𝐢𝐜𝐬═════⌯──╮ \n├⚡ {progress_bar} |﹝{perc}﹞ \n├🚀 Speed ➤ {sp} \n├📟 Processed ➤ {cur}\n├🧲 Size - ETA ➤ {tot} - {eta} \n╰─═══ 🦋✨MAFIA✨🦋═══─╯`\n') 
+                await reply.edit(f"""`
+╔═══════════════════════╗
+┣⪼⚡ {progress_bar}|{perc}
+┣⪼🚀 SPEED {sp} 
+┣⪼📟 PROCESSED {cur}
+┣⪼🧲 SIZE-ETA {tot} -{eta} 
+╚═════[made by MΛFIΛ☣]════╝`""") 
             except FloodWait as e:
                 time.sleep(e.x)
 

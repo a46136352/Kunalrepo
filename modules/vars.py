8b57d1f68280ff6fa824ce3842393e51224646fd
ROWDY <excuseboy13@gmail.com>
2024-12-10 16:19:42 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27679306"
 api_hash = "d0f390f798eec4ddb9246a00a44b7a3f"
-bot_token = "8117656838:AAEBxICmjhCbcjTVJdQUxBWWdnwej4yNYQg"
+bot_token = "7920882773:AAH5a0vGckEqrRW1GQc-3nIauIWf6pwtiEc"

a329bca848a00ad9003e32d3c8a80cef7e352260
Bkphere <163238356+Bkphere@users.noreply.github.com>
2024-12-10 21:01:30 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "27679306"
 api_hash = "d0f390f798eec4ddb9246a00a44b7a3f"
-bot_token = "7920882773:AAH5a0vGckEqrRW1GQc-3nIauIWf6pwtiEc"
+bot_token = "7999676508:AAGEHqgOGthH10dv2WckrqEvBAGeepT4v04"
